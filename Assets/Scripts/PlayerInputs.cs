8d3c754908753be3bbfe37548a677c9849605e45
mln-grge <mgeorge00747@gmail.com>
2020-11-04 13:12:37 +0530

    Added a Player Controller,camera controller and a separete input script for handling player inputs.

---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour
+{
+    private float VerticalInput;
+    public float verticalinput { get { return VerticalInput; } }
+    
+    private float HorizontalInput;
+    public float horizontalinput { get { return HorizontalInput; } }
+    
+    private bool isBreaking;
+    public bool isbreaking { get { return isBreaking; } }
+    
+    void FixedUpdate()
+    {
+        VerticalInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+}
